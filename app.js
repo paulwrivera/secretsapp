b18c8321269803d6c61a9b5e0265fa91156d03ec
Paul Rivera <itspaulrivera@gmail.com>
2022-08-31 12:58:26 -0700

    Add secret submission to database and secret display on 'secrets' page. Remove login requirement to see secrets.

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 ////////////////////////////////////////////// User schema (blueprint) plugins /////////////////////////////////////////////
@@ -137,8 +138,20 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+    User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers });
+            }
+        }
+    });
+});
+
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
@@ -189,6 +202,23 @@ app.post("/login", function (req, res) {
         }
     });
 
+});
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets")
+                });
+            }
+        }
+    });
 });
 
 /////////////////////////////////////////////////////// Launch server //////////////////////////////////////////////////////
